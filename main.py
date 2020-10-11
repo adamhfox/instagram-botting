04609b73419968e7ae90940d7a7adae150d7a3b4
adamhfox <adamhfox624@gmail.com>
2020-10-11 16:19:25 -0400

    Logs into instagram and uploads at correct time

--- main.py
+++ main.py
@@ -1,39 +1,20 @@
-# imports
-from instapy import InstaPy
-from instapy import smart_run
+from instabot import Bot
+import schedule
 import time
-import  random
 
-# login credentials
-insta_username = ''  # <- enter username here
-insta_password = ''  # <- enter password here
 
-#hashtag targeting
-hashtags = ['']]
+def upload_photo(username1, password1, photo, the_caption):
+    bot = Bot()
+    bot.login(username=username1,password=password1)
+    bot.upload_photo(photo, the_caption)
 
-# target list
-target_list = ['']
-# get an InstaPy session!
-# set headless_browser=True to run InstaPy in the background
-session = InstaPy(username=insta_username,
-                  password=insta_password,
-                  headless_browser=False)
 
-with smart_run(session):
-    """ Activity flow """
-    # general settings
-    session.set_relationship_bounds(enabled=True,
-                                    delimit_by_numbers=True,
-                                    max_followers=4590,
-                                    min_followers=45,
-                                    min_following=77)
 
-    
 
-    # activity
-    i = 0
-    while i < 3:
-        session.follow_user_followers(target_list, amount=35, randomize=False, interact=False, sleep_delay=random.randint(60, 75))
-        session.like_by_tags(random.sample(hashtags, 4), amount=random.randint(1,20))
-        i += 1
-    
+
+
+schedule.every().day.at("16:17").do(upload_photo, )
+
+while True:
+    schedule.run_pending()
+    time.sleep(60) 
